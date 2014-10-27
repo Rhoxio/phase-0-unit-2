25da8b26a39dd1b27411127a72ab3efd2768710e
Kevin <azorius07@yahoo.com>
2014-10-26 22:26:27 -0700

    Update my_solution.rb

--- week-6/4-bingo-2-solo-challenge/my_solution.rb
+++ week-6/4-bingo-2-solo-challenge/my_solution.rb
@@ -2,6 +2,11 @@
 
 
 # 1. Pseudocode
+=begin
+	
+You can check the rows of each 
+	
+=end
 
 # sample_board =[[47, 44, 71, 8, 88],
 #                [22, 69, 75, 65, 73],
@@ -13,49 +18,97 @@
 
 
 # 2. Initial Solution
-@b = ['x', 44, 71, 8, 88]
-@i = [22, 'x', 75, 65, 73]
-@n = [83, 85, 'x', 89, 57]
-@g = [25, 31, 96, 'x', 51]
-@o = [75, 70, 54, 80, 'x']
+
+class BingoBoard
+
+  attr_reader :row_check
+
+  def initialize(sel, num, bingo_board)
+    #@bingo_board = Array.new(5) {Array(5.times.map{rand(1..100)})}
+    @bingo_board = bingo_board
+    @num = num
+    @sel = sel.to_s.downcase
+
+    #@b = ['x', 44, 71, 8, 88]
+	#@i = [22, 'x', 75, 65, 73]
+	#@n = [83, 85, 'x', 89, 57]
+	#@g = [25, 31, 96, 'x', 51]
+	#@o = [75, 70, 54, 80, 'x']
+  
+    @b = @bingo_board.collect {|index| index[0]}
+    @i = @bingo_board.collect {|index| index[1]}
+    @n = @bingo_board.collect {|index| index[2]}
+    @g = @bingo_board.collect {|index| index[3]}
+    @o = @bingo_board.collect {|index| index[4]}
+  end
+
 
 #columns = Array.new
 #all_columns = columns.push(@b,@i,@n,@g,@o)
 
 
-public
-def row_check?
-    if self.uniq.length == 1 #could also just do #count % 5 == 0.
+
+
+  def manipulation #original method to check for a matching slot. 
+    if @b.include? @num and @sel == "b"
+    puts "You got #{@sel.upcase} #{@num}!"
+    @b.map! {|x| (x == @num) ? "x" : x}
+  elsif @i.include? @num and @sel == "i"
+    puts "You got #{@sel.upcase} #{@num}!"
+    @i.map! {|x| (x == @num) ? "x" : x}
+  elsif @n.include? @num and @sel == "n"
+    puts "You got #{@sel.upcase} #{@num}!"
+    @n.map! {|x| (x == @num) ? "x" : x}
+  elsif @g.include? @num and @sel == "g"
+    puts "You got #{@sel.upcase} #{@num}!"
+    @g.map! {|x| (x == @num) ? "x" : x}
+  elsif @o.include? @num and @sel == "o"
+    puts "You got #{@sel.upcase} #{@num}!"
+    @o.map! {|x| (x == @num) ? "x" : x}
+  else
+    puts "Not on the board! Try again!"
+  end
+  p @b
+  p @i
+  p @n
+  p @g
+  p @o
+ end
+
+  public
+  def row_check #this method can be used to check arrays for all matching instances of any character. I added it to attr_reader so it can be read from everywhere. 
+    if self.uniq.length == 1 #could also just do #count % 5 == 0 for this particular problem.
       return true
     else
       return false
     end
-end
+  end
 
-def row_ans
-	if @b.row_check? == true
+  def row_ans
+	if @b.row_check == true
 		puts "true"
 		return true
-	elsif @i.row_check? == true
+	elsif @i.row_check == true
 		puts "true"
 		return true
-	elsif @n.row_check? == true
+	elsif @n.row_check == true
 		puts "true"
 		return true
-	elsif @g.row_check? == true
+	elsif @g.row_check == true
 		puts "true"
 		return true
-	elsif @o.row_check? == true
+	elsif @o.row_check == true
 		puts "true"
 		return true
  	else
  		puts "false"
  		return false
 	end
-end
+  end
 
-def column_check
-	columns = Array.new
+
+  def column_check
+	columns = []
 	all_columns = columns.push(@b,@i,@n,@g,@o)
 
  	b = all_columns.collect {|index| index[0]}
@@ -64,28 +117,28 @@ def column_check
  	g = all_columns.collect {|index| index[3]}
  	o = all_columns.collect {|index| index[4]}
 
-	if b.row_check? == true
+	if b.row_check == true
 		puts "true"
 		return true
-	elsif i.row_check? == true
+	elsif i.row_check == true
 		puts "true"
 		return true
-	elsif n.row_check? == true
+	elsif n.row_check == true
 		puts "true"
 		return true
-	elsif g.row_check? == true
+	elsif g.row_check == true
 		puts "true"
 		return true
-	elsif o.row_check? == true
+	elsif o.row_check == true
 		puts "true"
 		return true
  	else
  		puts "incorrect"
  		return false
 	end
-end
+  end
 
-def check_diagonals
+  def check_diagonals
 	 if @b[0] == "x" && @i[1] == "x" && @n[2] == "x" && @g[3] == "x" && @o[4] == "x"
 	 	puts "true"
 	 elsif @b[4] == "x" && @i[3] == "x" && @n[2] == "x" && @g[1] == "x" && @o[0] == "x"
@@ -93,12 +146,7 @@ def check_diagonals
 	 else
 	 	puts "false"
 	 end
-end
-
-check_diagonals
-
-  #your code here
-
+  end
 end
 
 
@@ -106,14 +154,57 @@ end
 
 # 4. Refactored Solution
 
-
-
-
-
+=begin
+	
+For the most part, the code can be broken down more by using blocks to select the element in the nested array, then choosing the
+element in the element as the output. I thought my way was easier to handle logically, though. One thing that I
+have noticed this week is that sometimes code isn't better simply because it is shorter. Sure, you can chain 
+together a thousand methods to get an answer, but if someone looking at it sees it as a mass of jargon
+with no flow of logic, you can't call it "good" code. I ammended the #column_check a bit, as it created too many
+variables before. I decided to apply the concepts of scope and make local variables as opposed to using class or instance variables
+for everything. 
+	
+=end
 
 # 3. DRIVER TESTS GO BELOW THIS LINE
-# create driver test code to retrieve a value at a coordinate here:
-# implement tests for each of the methods here:
+test = BingoBoard.new("b", 47, [[47, 44, 71, 8, 88],
+   					           ['x', 'x', 'x', 'x', 'x'],
+   							   [83, 85, 97, 89, 57],
+   							   [25, 31, 96, 68, 51],
+   							   [75, 70, 54, 80, 83]]).manipulation #this is a simple check for matching values.
+test2 = BingoBoard.new("b", 63, [[47, 44, 71, 'x', 88],
+   [22, 69, 75, 'x', 73],
+   [83, 85, 97, 'x', 57],
+   [25, 31, 96, 'x', 51],
+   [75, 70, 54, 'x', 83]]).column_check #these three should ignore the inputs. They are only there so it doesn't throw argument errors. 
+test3 =  BingoBoard.new("b", 63, [['x', 44, 71, 8, 88],
+   [22, 'x', 75, 65, 73],
+   [83, 85, 'x', 89, 57],
+   [25, 31, 96, 'x', 51],
+   [75, 70, 54, 80, 'x']]).check_diagonals 
+test4 =  BingoBoard.new("b", 63, [[47, 44, 71, 8, 88],
+   ['x', 'x', 'x', 'x', 'x'],
+   [83, 85, 97, 89, 57],
+   [25, 31, 96, 68, 51],
+   [75, 70, 54, 80, 83]]).row_ans
+
+test
+test2
+test3
+test4 
+
+# As a side note, if a puts gives you "true", it is the same as a "bingo".
 
 
 # 5. Reflection
+=begin
+	
+I realize that my solution essentially curcumvents the need to deal with nested arrays all the time, but I felt
+like this code was much easier to read and devise a solution for once it was broken down in to smaller 
+pieces instead of dealing with "invisible" values all over the place. It is true that you can break it down
+a bit more by actually accessing the nested arrays and pulling values out of them directly, but again, I felt like having the 
+code broken down not only made it eaiser for people who saw my code to understand what it is doing, but it made me understand how the process worked
+more fluidly. Methods tend to hide your logic, and while they are great for simple refactoring, they are not always 
+indicative of a better understanding of code. 
+	
+=end
